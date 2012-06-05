71820f72cfb3795d22962d36fa71324686e08ce8
evx001 <evx001@evx001-VirtlBox.(none)>
2012-06-05 19:28:16 -0400

    1st commit company--> Sam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EVxCMS::Application.config.secret_token = '25f147033bcd90d95acb32e645daf4720bb11b862be7a57a703b87019290dec6677695e0d4fd8c0dfeea8f5c535220949f5cf8cf60e2b4b5189f74709bbc8cca'
